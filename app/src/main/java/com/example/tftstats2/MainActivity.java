c19658b288daef9830073991903043f0c248a155
BuildTools <unconfigured@null.spigotmc.org>
2020-06-10 10:20:55 +0900

    테스트용 커밋

--- app/src/main/java/com/example/tftstats2/MainActivity.java
+++ app/src/main/java/com/example/tftstats2/MainActivity.java
@@ -1,54 +1,30 @@
 package com.example.tftstats2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FragmentManager fragmentManager = getSupportFragmentManager();
-    private Fragment_Search fragment_search = new Fragment_Search();
-    private Fragment_Save fragment_save = new Fragment_Save();
-    private Fragment_Profile fragment_profile = new Fragment_Profile();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //주석 처리된 부분은 하단바 작동 부분임
-       /* FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameLayout, fragment_search).commitAllowingStateLoss();
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());*/
     }
 
-   /* class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-            switch(menuItem.getItemId())
-            {
-                case R.id.searchItem:
-                    transaction.replace(R.id.frameLayout, fragment_search).commitAllowingStateLoss();
-                    break;
-                case R.id.cameraItem:
-                    transaction.replace(R.id.frameLayout, fragment_save).commitAllowingStateLoss();
-                    break;
-                case R.id.callItem:
-                    transaction.replace(R.id.frameLayout, fragment_profile).commitAllowingStateLoss();
-                    break;
+   /* public view clickMethod(view view)
+        Imageview b = (Imageview) findViewById(R.id.loginbtn);
+        b.setOnClickListener(new b.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        bottomview_ctr.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
             }
-            return true;
-        }
+        });
     }*/
 }
